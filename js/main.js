f2d0694d0609ba834ebb2a06dcc9e3138ca2573e
Carlos Sosa <carlossosa54321@gmail.com>
2022-04-14 14:33:17 -0700

    added files from local git to new remote repo

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//document.querySelector('button').addEventListener('click', getFetch)
+
+//function getFetch(){
+  //const choice = document.querySelector('input').value
+  const url = `https://www.breakingbadapi.com/api/character/random`
+  const url2 = `https://www.breakingbadapi.com/api/quotes`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('h1').innerText = data[0].name
+        document. querySelector('img').src = data[0].img
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+
+    let i = Math.ceil(Math.random()*70)
+
+  fetch(url2)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('p').innerText = data[i].quote
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+//}
